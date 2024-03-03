25a887d3dfe88489af4471e0e29987a67fab2e07
ElyesBenkhoud <hechemferchiou@gmail.com>
2024-03-03 19:27:20 +0200

    sparkles added and error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import Chat from "./components/Chat";
 import Header from "./components/Header";
 import QnA from "./components/QnA";
+import { SparklesPreview } from "./components/SparklesPreview";
 
 function App() {
   return (
@@ -13,12 +14,14 @@ function App() {
     //make it responsive such in smaller screens chat goes below the suggestion
     <div>
       <Header />
+      {/* </div> */}
       <div className="content">
         <div>
           <QnA />
         </div>
         <div>{/* <Chat /> */}</div>
       </div>
+      <SparklesPreview />
     </div>
   );
 }
