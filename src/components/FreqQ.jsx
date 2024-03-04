2f3bc8766cc6989f55dea20685db717c866e05cb
ElyesBenkhoud <elyesbenkhoud377@gmail.com>
2024-03-04 20:06:48 +0200

    frequently question style fixed

--- src/components/FreqQ.jsx
+++ src/components/FreqQ.jsx
@@ -2,20 +2,22 @@ import React from "react";
 
 function FreqQ() {
   return (
-    <div className="bg-white dark:bg-gray-900 dark:text-gray-100">
+    <div className="bg-gray ">
       <div className="container mx-auto space-y-16 px-4 py-16 lg:px-8 lg:py-32 xl:max-w-7xl">
         <div className="text-center">
-          <div className="mb-1 text-sm font-bold uppercase tracking-wider text-blue-600 dark:text-blue-500">
+          <div className="mb-1 text-sm font-bold uppercase tracking-wider text-gray-600 ">
             We Answer
           </div>
-          <h2 className="text-4xl font-black text-black dark:text-white">
+          <h2 className="text-4xl font-black text-black ">
             Frequently Asked Questions
           </h2>
         </div>
 
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 md:gap-12">
           <div>
-            <h4 className="mb-2 font-semibold">What features are included?</h4>
+            <h4 className="mb-2 font-semibold text-gray-800">
+              What features are included?
+            </h4>
             <p className="leading-relaxed text-gray-600 dark:text-gray-400">
               Etiam egestas fringilla enim, id convallis lectus laoreet at.
               Fusce purus nisi, gravida sed consectetur ut, interdum quis nisi.
@@ -23,7 +25,7 @@ function FreqQ() {
             </p>
           </div>
           <div>
-            <h4 className="mb-2 font-semibold">
+            <h4 className="mb-2 font-semibold text-gray-800">
               Do I get access to the community?
             </h4>
             <p className="leading-relaxed text-gray-600 dark:text-gray-400">
@@ -33,7 +35,7 @@ function FreqQ() {
             </p>
           </div>
           <div>
-            <h4 className="mb-2 font-semibold">
+            <h4 className="mb-2 font-semibold text-gray-800">
               Can I get a refund just in case?
             </h4>
             <p className="leading-relaxed text-gray-600 dark:text-gray-400">
@@ -43,7 +45,9 @@ function FreqQ() {
             </p>
           </div>
           <div>
-            <h4 className="mb-2 font-semibold">Do you offer email support?</h4>
+            <h4 className="mb-2 font-semibold text-gray-800">
+              Do you offer email support?
+            </h4>
             <p className="leading-relaxed text-gray-600 dark:text-gray-400">
               Etiam egestas fringilla enim, id convallis lectus laoreet at.
               Fusce purus nisi, gravida sed consectetur ut, interdum quis nisi.
@@ -55,27 +59,9 @@ function FreqQ() {
         <div className="text-center">
           <a
             href="javascript:void(0)"
-            className="inline-flex items-center justify-center space-x-2 rounded-lg border border-gray-200 bg-white px-4 py-2 font-semibold leading-6 text-gray-800 hover:border-gray-300 hover:text-gray-900 hover:shadow-sm focus:ring focus:ring-gray-300 focus:ring-opacity-25 active:border-gray-200 active:shadow-none dark:border-gray-700 dark:bg-gray-800 dark:text-gray-300 dark:hover:border-gray-600 dark:hover:text-gray-200 dark:focus:ring-gray-600 dark:focus:ring-opacity-40 dark:active:border-gray-700"
+            className="inline-flex items-center justify-center space-x-2 rounded-lg border border-gray-300 bg-yellow-400 px-7 py-3.5 font-semibold leading-6 text-black  hover:bg-gray-300 hover:text-black hover:border-gray-300 focus:ring focus:ring-yellow-400 focus:ring-opacity-50 "
           >
-            <svg
-              className="hi-mini hi-arrow-top-right-on-square inline-block size-5 opacity-50"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z"
-                clipRule="evenodd"
-              />
-              <path
-                fillRule="evenodd"
-                d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <span>Go to support center</span>
+            <span>Go to support</span>
           </a>
         </div>
       </div>
