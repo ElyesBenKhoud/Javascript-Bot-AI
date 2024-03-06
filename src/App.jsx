b8ea187f2b6a5d69cf00fe1fa49542233f3069c4
ElyesBenkhoud <elyesbenkhoud377@gmail.com>
2024-03-06 13:41:59 +0200

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
-import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+
 import LandingPage from "./pages/LandingPage";
-import QnA from "./components/QnA";
 import Header from "./components/UI/Header";
 import ChatComponent from "./pages/ChatComponent";
+
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import "./App.css";
 
 function App() {
   return (
