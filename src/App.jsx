505a18b3a11a1ffa0ee01ad572947a2f302576f4
ElyesBenkhoud <elyesbenkhoud377@gmail.com>
2024-03-22 21:55:26 +0200

    switcher mantine ui added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "@mantine/core/styles.css";
 
 import LandingPage from "./pages/LandingPage";
 import Header from "./components/UI/Header";
 import ChatComponent from "./pages/ChatComponent";
+import Switcher from "./components/UI/Switcher";
 
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import "./App.css";
@@ -18,6 +20,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/javascript-ai" element={<ChatComponent />} />
         </Routes>
+        <Switcher />
       </>
     </Router>
   );
