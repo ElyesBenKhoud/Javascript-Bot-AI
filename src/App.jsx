904844250580ec74f01a071ed62ebcf3f986fef1
ElyesBenkhoud <elyesbenkhoud377@gmail.com>
2024-03-25 19:35:38 +0200

    linting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "@mantine/core/styles.css";
 
 import LandingPage from "./pages/LandingPage";
 import Header from "./components/UI/Header";
 import ChatComponent from "./pages/ChatComponent";
 
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import "@mantine/core/styles.css";
 import "./App.css";
 
 function App() {
@@ -14,7 +14,6 @@ function App() {
     <Router>
       <>
         <Header />
-
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/javascript-ai" element={<ChatComponent />} />
